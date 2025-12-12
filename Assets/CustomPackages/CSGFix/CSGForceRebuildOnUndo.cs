ac728fdb17549c6a5dae8a36c0d3bb2afcf2d665
lizband <lizband.dev@gmail.com>
2025-12-12 12:47:36 -0500

    Added SaberCSG package, Added blockout for basic farm map (WIP)

---
+++ Assets/CustomPackages/CSGFix/CSGForceRebuildOnUndo.cs
@@ -0,0 +1,27 @@
+using Sabresaurus.SabreCSG;
+using UnityEditor;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CSGForceRebuildOnUndo : MonoBehaviour
+{
+//    public CSGModel csgModel;
+//    public bool buildInBackgroundThread = false;
+//#if UNITY_EDITOR
+//    private void OnEnable()
+//    {
+//        csgModel = GetComponent<CSGModel>();
+//        Undo.undoRedoPerformed += OnUndoOrRedo;
+//    }
+//    private void OnDisable()
+//    {
+//        Undo.undoRedoPerformed -= OnUndoOrRedo;
+//    }
+//
+//    private void OnUndoOrRedo()
+//    {
+//        //if (CSG_HotFix_Settings.for)
+//        csgModel.Build(true, buildInBackgroundThread);
+//    }
+//#endif
+}
